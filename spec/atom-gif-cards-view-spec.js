10082c84c3a6396a3b69b7bc1fe9151bc40705cb
Eric Adamski <er.adamski@gmail.com>
2018-03-13 15:03:21 -0400

    Initial commit!

---
+++ spec/atom-gif-cards-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import AtomGifCardsView from '../lib/atom-gif-cards-view';
+
+describe('AtomGifCardsView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
